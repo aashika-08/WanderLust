c48c87de163ee75980fd1b8d6e5d2a39a1af23b0
aashika-08 <aashikajain0726@gmail.com>
2024-10-04 00:24:07 +0530

    Uploading files

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const mongoose = require("mongoose");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
 const MongoStore = require('connect-mongo');
 const flash = require("connect-flash");
@@ -17,7 +18,6 @@ const User = require("./models/user.js");
 const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
-const ExpressError = require("./utils/ExpressError.js");
 
 const dbUrl = process.env.ATLASDB_URL;
 
@@ -30,7 +30,7 @@ main()
     });
 async function main() {
     await mongoose.connect(dbUrl);
-}
+};
 
 app.set("view-engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -84,23 +84,9 @@ app.use((req, res, next) => {
     next();
 });
 
-// app.get("/demouser", async (req, res) => {
-//     let fakeUser = new User({
-//         email: "student@gmail.com",
-//         username: "sigma-student"
-//     });
-
-//     let registeredUser = await User.register(fakeUser, "helloworld");
-//     res.send(registeredUser);
-// });
-
 app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
-
-// app.all("*", (req, res, next) => {
-//     next(new ExpressError(404, "Page Not Found!"));
-// });
 
 app.listen(8080, () => {
     console.log("server is listening to port 8080");
